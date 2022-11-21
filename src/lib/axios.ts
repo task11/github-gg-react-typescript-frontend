8eb839f4029acf6a16af7a910c985f9f3fc7bf6c
task11 <task11@naver.com>
2022-11-21 10:59:11 +0900

    feat: add axios instance

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,1 +1,13 @@
-export const axios = '';
+import axios from 'axios';
+
+const API_URL = 'https://api.github.com';
+
+const server = axios.create({
+  baseURL: API_URL,
+  headers: {
+    authorization: `Bearer ${process.env.REACT_APP_TOKEN}`,
+    'Content-Type': 'application/vnd.github+json',
+  },
+});
+
+export default server;
