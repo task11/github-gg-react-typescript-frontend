0000c9dc1b954a99676cd54afbd23d14636dd2fb
task11 <task11@naver.com>
2022-11-22 23:37:53 +0900

    feat: declare react query cache type & cache query

---
+++ src/types/queryData.ts
@@ -0,0 +1,7 @@
+export type QueryDataPropsList = QueryDataProps[];
+
+export interface QueryDataProps {
+  id?: number;
+  avatarUrl?: string;
+  username: string;
+}
--- src/utils/models/test.ts
+++
@@ -1,6 +0,0 @@
-const TEST_KEY = 'getTest';
-
-export const todoCache = {
-  getTestAll: TEST_KEY,
-  getTestById: (testId: string) => TEST_KEY + testId,
-};
