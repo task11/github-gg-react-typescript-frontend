e698c87ce054ae0bfedc6d4c66984e34c10e2ce3
task11 <task11@naver.com>
2022-11-22 23:35:42 +0900

    feat: add useSearch hook

---
+++ src/feature/Search/api/useSearch.tsx
@@ -0,0 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
+
+import server from '../../../lib/server';
+
+import { SearchUserResponseProps } from '../../../types/user';
+import { userCache } from '../../../utils/models/userCache';
+
+export default function useSearch(query: string) {
+  const getUsersByQuery = async () => {
+    return (
+      await server.get<SearchUserResponseProps>(
+        `/search/users?q=${query}&per_page=5&page=1`,
+      )
+    ).data;
+  };
+
+  return useQuery(userCache.getUsersByQuery(query), getUsersByQuery, {
+    keepPreviousData: true,
+    refetchOnWindowFocus: true,
+    staleTime: 60000,
+    enabled: !!query,
+  });
+}
