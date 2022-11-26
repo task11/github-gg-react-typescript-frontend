6b2769f477664b784ccfb081a462d0187c5b5ad8
task11 <task11@naver.com>
2022-11-26 21:23:58 +0900

    feat: add search page route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Main from './layouts/Main/Main';
+
 import HomePage from './pages/HomePage/HomePage';
+import SearchPage from './pages/SearchPage/SearchPage';
 import NotFountPage from './pages/NotFoundPage/NotFountPage';
 
 export default function Router() {
@@ -10,6 +12,7 @@ export default function Router() {
       <Routes>
         <Route element={<Main />}>
           <Route path="/" element={<HomePage />} />
+          <Route path="/search" element={<SearchPage />} />
         </Route>
         <Route path="*" element={<NotFountPage />} />
       </Routes>
