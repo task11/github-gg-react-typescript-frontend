f1c6c6b372d57f6d302122eb3bfd8893dcc4c79b
task11 <task11@naver.com>
2022-11-29 18:15:39 +0900

    feat: add user page

---
+++ src/pages/UserPage/UserPage.tsx
@@ -0,0 +1,11 @@
+import UserContainer from '../../feature/User/components/UserContainer';
+
+import { StyledUser } from './UserPage.style';
+
+export default function UserPage() {
+  return (
+    <StyledUser>
+      <UserContainer />
+    </StyledUser>
+  );
+}
