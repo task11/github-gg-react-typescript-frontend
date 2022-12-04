bd8d15c893b8c98e753bc9818be31bb93c16533d
task11 <task11@naver.com>
2022-12-05 01:03:07 +0900

    fix: add z index in header

--- src/layouts/Header/Header.style.tsx
+++ src/layouts/Header/Header.style.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const StyledHeader = styled.header`
   position: sticky;
   top: 0;
+  z-index: 2;
   display: flex;
   justify-content: center;
   align-items: center;
