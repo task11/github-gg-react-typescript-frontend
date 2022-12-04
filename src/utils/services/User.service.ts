76a28bd6a0ca4e1c446200eeb3ac672343103811
task11 <task11@naver.com>
2022-12-05 01:10:53 +0900

    feat: user service

--- src/utils/services/User.service.ts
+++ src/utils/services/User.service.ts
@@ -1,16 +1,18 @@
 import server from '../../lib/axios';
 
+import { RepositoriesProps, UserProps } from '../../types';
+
 class UserService {
   getUser(username: string) {
-    return server.get(`/users/${username}`);
+    return server.get<UserProps>(`/users/${username}`);
   }
 
-  getUserRepositories(username: string) {
-    return server.get(`/users/${username}/repos`, {
+  getUserRepositories(username: string, page: number) {
+    return server.get<RepositoriesProps>(`/users/${username}/repos`, {
       params: {
         sort: 'updated',
         per_page: 4,
-        page: 0,
+        page,
       },
     });
   }
