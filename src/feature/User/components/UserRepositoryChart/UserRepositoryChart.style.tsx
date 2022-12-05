a543c613f2cac26b82acbccfb7fba776ae2fda95
task11 <task11@naver.com>
2022-12-05 22:19:43 +0900

    feat: add user repository detail info & chart

---
+++ src/feature/User/components/UserRepositoryChart/UserRepositoryChart.style.tsx
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+export const StyledUserRepositoryChart = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  width: 100%;
+  max-height: 500px;
+  background-color: ${(props) => props.theme.bgColor.card};
+  padding: 16px;
+  border-radius: 8px;
+  font-size: 12px;
+  font-weight: 600;
+`;
+
+export const StyledUserRepositoryInfo = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+
+  .repository-type {
+    color: ${(props) => props.theme.fontColor.accent};
+  }
+`;
+
+export const StyledRepositoryContent = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  gap: 6px;
+  background-color: ${(props) => props.theme.bgColor.background};
+  padding: 6px;
+  border-radius: 8px;
+
+  .label {
+    font-weight: 300;
+  }
+
+  .description {
+    line-height: 1.4;
+  }
+`;
+
+export const StyledChartWrapper = styled.div`
+  width: 100%;
+
+  .label {
+    text-align: center;
+    font-size: 12px;
+    font-weight: 300;
+    margin-bottom: 5px;
+  }
+`;
---
+++ src/feature/User/components/UserRepositoryChart/UserRepositoryChart.tsx
@@ -0,0 +1,78 @@
+import { useMemo } from 'react';
+import { useRecoilValue } from 'recoil';
+
+import Chart from 'react-apexcharts';
+
+import useUserRepository from '../../api/useUserRepository';
+
+import { RepositoryProps } from '../../../../types';
+
+import { themeAtom } from '../../../../store';
+
+import {
+  StyledRepositoryContent,
+  StyledUserRepositoryChart,
+  StyledUserRepositoryInfo,
+  StyledChartWrapper,
+} from './UserRepositoryChart.style';
+
+interface Props {
+  repository: RepositoryProps;
+}
+
+export default function UserRepositoryChart({ repository }: Props) {
+  const mode = useRecoilValue(themeAtom);
+  const { data, isLoading } = useUserRepository(
+    repository.owner.login,
+    repository.name,
+  );
+
+  const dataIsEmpty = useMemo(
+    () => data && Object.keys(data).length === 0 && data.constructor === Object,
+    [data],
+  );
+
+  if (isLoading) return <>loading..</>;
+
+  return (
+    <StyledUserRepositoryChart>
+      <StyledUserRepositoryInfo>
+        {repository.fork && (
+          <span className="repository-type">forked repository</span>
+        )}
+        {repository.description && (
+          <StyledRepositoryContent>
+            <span className="label">Description</span>
+            <strong className="description">{repository.description}</strong>
+          </StyledRepositoryContent>
+        )}
+        {repository.language && (
+          <StyledRepositoryContent>
+            <span className="label">Most used language</span>
+            <span>{repository.language}</span>
+          </StyledRepositoryContent>
+        )}
+      </StyledUserRepositoryInfo>
+      {!dataIsEmpty && (
+        <StyledChartWrapper>
+          <div className="label">Language chart</div>
+          <Chart
+            options={{
+              theme: {
+                mode,
+              },
+              chart: {
+                width: 380,
+                type: 'pie',
+              },
+              labels: Object.keys(data),
+            }}
+            series={Object.values(data) as number[]}
+            type="pie"
+            height="280px"
+          />
+        </StyledChartWrapper>
+      )}
+    </StyledUserRepositoryChart>
+  );
+}
