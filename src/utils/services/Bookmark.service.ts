b87b73e4079c02391ad40017d6b9fab8d4c1e6ad
task11 <task11@naver.com>
2022-12-07 21:20:49 +0900

    feat: bookmark service method

--- src/utils/services/Bookmark.service.ts
+++ src/utils/services/Bookmark.service.ts
@@ -3,15 +3,28 @@ import { QueryDataProps, QueryDataPropsList } from '../../types';
 import LocalService from './Local.service';
 
 class BookmarkService {
-  maxLength = 20;
+  maxLength = 8;
 
-  get(): QueryDataPropsList | null {
+  getBookmarks(): QueryDataPropsList | null {
     const data: QueryDataPropsList | null = LocalService.get('bookmark');
+
     if (!data) {
       return null;
     }
 
     return data;
+  }
+
+  getBookmark(userId: number) {
+    const data: QueryDataPropsList | null = LocalService.get('bookmark');
+
+    if (!data) {
+      return null;
+    }
+
+    const [user] = data.filter(({ id }) => userId === id);
+
+    return user;
   }
 
   set(data: QueryDataProps) {
@@ -22,16 +35,41 @@ class BookmarkService {
       return;
     }
 
-    const dataWithoutDuplicate = this.removeDuplicate(currentData, data);
+    const isDuplicate = currentData.some(({ id }) => data.id === id);
 
-    this.lengthCheck(dataWithoutDuplicate);
-    this.arrayResize(dataWithoutDuplicate);
+    if (isDuplicate) {
+      const dataWithoutDuplicate = currentData.filter(
+        ({ username }) => username !== data.username,
+      );
 
-    LocalService.set('bookmark', [...dataWithoutDuplicate, data]);
+      LocalService.set('bookmark', dataWithoutDuplicate);
+      return;
+    }
+
+    this.arrayResize(currentData);
+
+    LocalService.set('bookmark', [...currentData, data]);
   }
 
-  removeDuplicate(currentData: QueryDataPropsList, data: QueryDataProps) {
-    return currentData.filter(({ username }) => username !== data.username);
+  removeBookmark(userId: number) {
+    const currentData: QueryDataPropsList | null = LocalService.get('bookmark');
+
+    if (!currentData) {
+      return null;
+    }
+    const newData = currentData.filter(({ id }) => id !== userId);
+
+    LocalService.set('bookmark', newData);
+  }
+
+  getTotalCount() {
+    const data: QueryDataPropsList | null = LocalService.get('bookmark');
+
+    if (!data) {
+      return 0;
+    }
+
+    return data.length;
   }
 
   lengthCheck(data: QueryDataPropsList) {
