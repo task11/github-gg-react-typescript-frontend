ef788838cfa2f40e963d019e160c231601ba1678
task11 <task11@naver.com>
2022-12-08 19:31:23 +0900

    refactor: add border top color theme

--- src/feature/SearchForm/components/SearchRecentList/SearchRecentList.style.tsx
+++ src/feature/SearchForm/components/SearchRecentList/SearchRecentList.style.tsx
@@ -10,7 +10,7 @@ export const StyledRecentSearchWrapper = styled.section`
     font-size: 14px;
     color: rgb(106, 115, 123);
     padding-top: 12px;
-    border-top: 1px solid rgba(187, 187, 187, 0.69);
+    border-top: 1px solid ${(props) => props.theme.borderColor.top};
   }
 `;
 
@@ -35,4 +35,8 @@ export const StyledRecentItem = styled.button`
   font-weight: 500;
   box-shadow: rgba(30, 32, 37, 0.1) 0px 2px 10px;
   cursor: pointer;
+
+  &:visited {
+    color: '#121212';
+  }
 `;
