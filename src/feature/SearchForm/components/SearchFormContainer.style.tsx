fc5c56e94f021c8f1a2becf3d6796920ad81fa97
task11 <task11@naver.com>
2022-12-09 11:09:48 +0900

    fix: search form width bug

--- src/feature/SearchForm/components/SearchFormContainer.style.tsx
+++ src/feature/SearchForm/components/SearchFormContainer.style.tsx
@@ -3,4 +3,13 @@ import styled from 'styled-components';
 export const StyledSearchFormContainer = styled.section`
   display: flex;
   flex-direction: column;
+  width: 280px;
+
+  @media screen and (min-width: 768px) {
+    width: 300px;
+  }
+
+  @media screen and (min-width: 1024px) {
+    width: 320px;
+  }
 `;
--- src/feature/SearchForm/components/SearchResult/SearchResult.style.tsx
+++ src/feature/SearchForm/components/SearchResult/SearchResult.style.tsx
@@ -4,7 +4,7 @@ export const StyledSearchResult = styled.div`
   position: relative;
   width: 100%;
   max-height: 400px;
-  overflow-y: scroll;
+  overflow: hidden;
   background-color: white;
   border-radius: 15px;
   box-shadow: rgba(30, 32, 37, 0.1) 0px 2px 10px;
