73cc37a402875201e89b370a6c6637ac75ce6e0d
task11 <task11@naver.com>
2022-12-09 15:33:42 +0900

    test: useBookmark hook

---
+++ src/feature/Bookmark/hooks/__test__/useBookmark.test.tsx
@@ -0,0 +1,20 @@
+import { act, renderHook } from '@testing-library/react';
+
+import { RecoilRoot } from 'recoil';
+
+import useBookmark from '../useBookmark';
+
+describe('useBookmark', () => {
+  const renderer = () => {
+    const { result } = renderHook(() => useBookmark(), {
+      wrapper: ({ children }) => <RecoilRoot>{children}</RecoilRoot>,
+    });
+    return result;
+  };
+
+  it('Initial bookmarks state', () => {
+    const result = renderer();
+
+    expect(result.current.bookmarks).toStrictEqual([]);
+  });
+});
