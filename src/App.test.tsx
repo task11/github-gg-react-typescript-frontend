3ac8618f8dba6cc7d8f4b6807183922a67f40305
task11 <task11@naver.com>
2022-12-09 15:28:25 +0900

    refactor: app component test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,22 +1,27 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { render } from '@testing-library/react';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import { RecoilRoot } from 'recoil';
+
+import { CustomThemeProvider } from './styles';
 
 import App from './App';
 
 const queryClient = new QueryClient();
 
 describe('App', () => {
-  it('Shows App test', () => {
+  it('Shows App component', () => {
     const { queryByText } = render(
       <QueryClientProvider client={queryClient}>
         <RecoilRoot>
-          <App />
+          <CustomThemeProvider>
+            <App />
+          </CustomThemeProvider>
         </RecoilRoot>
       </QueryClientProvider>,
     );
 
-    expect(queryByText(/Task/)).not.toBeNull();
+    expect(queryByText(/Search for GitHub users/)).not.toBeNull();
   });
 });
