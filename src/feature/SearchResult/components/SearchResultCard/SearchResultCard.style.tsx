b4c0fabfd2fc2a2a6f9f457d9e7e806d575b91d6
task11 <task11@naver.com>
2022-12-09 16:09:36 +0900

    style: search result mediaquery

--- src/feature/SearchResult/components/SearchResultCard/SearchResultCard.style.tsx
+++ src/feature/SearchResult/components/SearchResultCard/SearchResultCard.style.tsx
@@ -9,10 +9,24 @@ export const StyledResultCard = styled.div`
   width: 100%;
   height: 56px;
   background-color: ${(props) => props.theme.bgColor.card};
-  transition: all 0.3s ease-in, color 0.2s ease-in;
-  border: 1px solid ${(props) => props.theme.bgColor.card};
+  transition: background 0.3s ease-in, color 0.2s ease-in;
   box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
   border-radius: 8px;
+  cursor: pointer;
+
+  svg {
+    fill: ${(props) => props.theme.fontColor.default};
+  }
+
+  @media screen and (min-width: 768px) {
+    height: 64px;
+    font-size: 18px;
+  }
+
+  @media screen and (min-width: 1024px) {
+    height: 72px;
+    font-size: 18px;
+  }
 `;
 
 export const StyledResultInfo = styled.div`
