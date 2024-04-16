2da4ae5aacd27b8f9c0fab021950b5394e563024
task11 <task11@naver.com>
2024-04-16 23:49:23 +0900

    style: adjust content layout

--- src/components/User/UserBookmarkButton/styles.ts
+++ src/components/User/UserBookmarkButton/styles.ts
@@ -1,9 +1,6 @@
 import styled from 'styled-components';
 
 export const StyledBookmark = styled.div`
-  position: absolute;
-  top: 4px;
-  right: 0;
   cursor: pointer;
 
   svg {
--- src/layouts/Content/Content.style.tsx
+++ src/layouts/Content/Content.style.tsx
@@ -3,4 +3,6 @@ import styled from 'styled-components';
 export const StyledContent = styled.section`
   width: 100%;
   padding: 36px;
+  display: flex;
+  justify-content: center;
 `;
